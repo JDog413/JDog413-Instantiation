cdce5d9ffca73bc1ac482b4b656e02337b710a13
J Dog413 <kj398155@gmail.com>
2023-03-06 18:33:53 +0000

    instantiateANewObject will return a String object.

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,6 +18,7 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        String st = new String();
+        return st;
     }
 }
